56d27b24fdf2ebfb6a0cb4b14748a7bbe79b43ed
xuyiling <dumi054054@outlook.com>
2026-01-06 17:59:08 +0100

    new begin

--- core/src/de/tum/cit/fop/maze/GameScreen.java
+++ core/src/de/tum/cit/fop/maze/GameScreen.java
@@ -112,6 +112,7 @@ public class GameScreen implements Screen {
         initCommon();
     }
 
+
     private void initCommon() {
         settingsManager = new SettingsManager();
         camera = new OrthographicCamera();
