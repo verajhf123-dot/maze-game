571162da73102269be7de6e92dbcc7c070e85fba
Jiao Hongfei <verajhf123@gmail.com>
2026-01-22 10:58:44 +0100

    mapandcharacter

--- core/src/de/tum/cit/fop/maze/MapLoader.java
+++ core/src/de/tum/cit/fop/maze/MapLoader.java
@@ -50,15 +50,13 @@ public class MapLoader {
         Properties props = new Properties();
 
         try {
-            FileHandle file = Gdx.files.local(internalPath);
+            FileHandle file = Gdx.files.internal(internalPath);
             if (!file.exists()) {
-                file = Gdx.files.internal(internalPath);
-                if (!file.exists()) {
-                    System.err.println("Map file not found: " + internalPath);
-                    return data;
-                }
+                System.err.println("Map file not found: " + internalPath);
+                return data;
             }
             props.load(file.read());
+
 
             for (String key : props.stringPropertyNames()) {
                 if (!key.contains(",")) continue;
